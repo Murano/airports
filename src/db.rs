206dc3ca5a461bbfd7bb57dbe98bf88995ff4daa
murano <murano1980@gmail.com>
2018-03-12 01:05:18 +0300

    Fixed 3 depth

--- src/db.rs
+++ src/db.rs
@@ -61,24 +61,24 @@ pub struct SearchRequest {
 
 #[derive(Serialize, Debug)]
 pub struct Solution {
-    ticket_ids: RefCell<Vec<String>>,
+    ticket_ids: Vec<String>,
     price: f32
 }
 
 
 #[derive(Serialize, Debug)]
 pub struct Solutions {
-    solutions: RefCell<Vec<Solution>>
+    solutions: Vec<Solution>
 }
 
 
 impl Solutions {
-    fn merge(&self, other: Solutions) {
-        self.solutions.borrow_mut().append(&mut other.solutions.borrow_mut());
+    fn merge(&mut self, mut other: Solutions) {
+        self.solutions.append(&mut other.solutions);
     }
 
     fn is_empty(&self) -> bool {
-        self.solutions.borrow().is_empty()
+        self.solutions.is_empty()
     }
 }
 
@@ -113,7 +113,7 @@ impl Database {
 
         match self.airports.borrow().get(&req.departure_code) {
             Some(airport_ref) => {
-                let solutions = self.check_tickets(req, airport_ref, None);
+                let solutions = self.check_tickets(req, airport_ref, vec![]);
 
                 if solutions.is_empty() {
                     Err("Tickets not found")
@@ -126,14 +126,13 @@ impl Database {
 
     }
 
-    fn check_tickets(&self, req: &SearchRequest, airport: &Airport, opt_rc_tickets_chain: Option<RefCell<Vec<Ticket>>>) -> Solutions {
-        let solutions = Solutions { solutions: RefCell::new(vec![]) };
-        //ссылки на тикеты, которые будут учавствовасть в поиске в глубину
+    fn check_tickets(&self, req: &SearchRequest, airport: &Airport, mut tickets_chain: Vec<Ticket>) -> Solutions {
+        let mut solutions = Solutions { solutions: vec![] };
 
+        //ссылки на тикеты, которые будут учавствовать в поиске в глубину
         for ticket in airport.directions.borrow().iter() {
-            match opt_rc_tickets_chain {
+            if tickets_chain.is_empty() {
                 //первый вызов check_tickets
-                None => {
 
                     if ticket.departure_time < req.departure_time_start || ticket.departure_time > req.departure_time_end {
                         continue; //не входит в промежуток времени
@@ -142,30 +141,27 @@ impl Database {
                     //есть вероятность того что этот билет приведет туда где найдутся варианты для стыковки
                     if ticket.arrival_code != req.arrival_code {
                         if let Some(airport_ref) = self.airports.borrow().get(&ticket.arrival_code) {
-                            let solutions_1 = self.check_tickets(req, airport_ref, Some(RefCell::new(vec![ticket.clone()])));
+                            let solutions_1 = self.check_tickets(req, airport_ref, vec![ticket.clone()]);
                             solutions.merge(solutions_1)
                         }
                     } else {
                         let solution = Solution {
-                            ticket_ids: RefCell::new(vec![ticket.id.clone()]),
+                            ticket_ids: vec![ticket.id.clone()],
                             price: ticket.price
                         };
 
-                        solutions.solutions.borrow_mut().push(solution);
+                        solutions.solutions.push(solution);
                     }
-                }
-                //ради упрощения сделан поиск максимум с пятью стыковками
-                Some(ref tickets_chain) => {
-                    let chain_length = tickets_chain.borrow().len();
+                } else {
+                    let chain_length = tickets_chain.len();
                     if chain_length == 5 {
                         break;
                     }
 
-                    //проверяем билет по критерию времени пересадки 3<t<8
 
-                    let prev_ticket = tickets_chain.borrow().last().unwrap().to_owned();
-//                    println!("Prev {:?}", prev_ticket);
-//                    println!("Current {:?}", ticket);
+                    let prev_ticket = tickets_chain.last().unwrap().to_owned();
+
+                    //проверяем билет по критерию времени пересадки 3<t<8
                     if prev_ticket.arrival_time + 3 * 60 * 60 > ticket.departure_time  ||
                         ticket.departure_time > prev_ticket.arrival_time + 8 * 60 * 60 {
                         continue; //билет не подходит ни для стыковки ни для дальнейших поисков
@@ -176,7 +172,7 @@ impl Database {
 
                         let mut ticket_ids = vec![];
                         let mut price: f32 = 0.0;
-                        for ticket_from_chain in tickets_chain.borrow().iter() {
+                        for ticket_from_chain in tickets_chain.iter() {
                             ticket_ids.push(ticket_from_chain.id.clone());
                             price += ticket_from_chain.price
                         }
@@ -185,22 +181,23 @@ impl Database {
                         price += ticket.price;
 
                         let solution = Solution {
-                            ticket_ids: RefCell::new(ticket_ids),
+                            ticket_ids,
                             price
                         };
 
-                        solutions.solutions.borrow_mut().push(solution);
+                        solutions.solutions.push(solution);
                         continue;
                     }
 
-                    if let Some(airport_ref) = self.airports.borrow().get(&ticket.departure_code) {
-                        tickets_chain.borrow_mut().push(ticket.to_owned());
+                    if let Some(airport_ref) = self.airports.borrow().get(&ticket.arrival_code) {
+                        tickets_chain.push(ticket.to_owned());
                         let solutions_1 = self.check_tickets(
-                            req, airport_ref, Some(RefCell::new(tickets_chain.borrow_mut().to_owned()))
+                            req, airport_ref, tickets_chain.to_owned()
                         );
-                        solutions.merge(solutions_1)
+                        solutions.merge(solutions_1);
+                        tickets_chain.pop();
                     }
-                }
+
             }
 
         }
